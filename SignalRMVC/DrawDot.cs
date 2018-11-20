c343eab31713b68d9a7b50ef12c40a1b3cd2dd9b
idellobel <ivan.dellobel@gmail.com>
2018-11-20 18:47:20 +0100

    Project SignalR MVC

---
+++ SignalRMVC/DrawDot.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+namespace SignalRMVC
+{
+    public class DrawDot : Hub
+    {
+        public void UpdateCanvas(int x, int y)
+        {
+            Clients.All.updateDot(x, y);
+        }
+        public void ClearCanvas()
+        {
+            Clients.All.clearCanvas();
+        }
+    }
+}
